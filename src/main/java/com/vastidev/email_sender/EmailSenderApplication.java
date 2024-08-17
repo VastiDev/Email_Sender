585e5f8bd468f628c47891c3250f1a3ac81e1bc1
VastiDev <vastiane1@gmail.com>
2024-08-17 10:06:36 -0300

    add welcome message

--- src/main/java/com/vastidev/email_sender/EmailSenderApplication.java
+++ src/main/java/com/vastidev/email_sender/EmailSenderApplication.java
@@ -2,10 +2,17 @@ package com.vastidev.email_sender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmailSenderApplication {
 
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Email Sender Api.";
+    }
     public static void main(String[] args) {
         SpringApplication.run(EmailSenderApplication.class, args);
     }
